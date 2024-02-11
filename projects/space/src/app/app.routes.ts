3f48bc41527c1aa0244b48e9cf48564f5570ea44
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2024-02-11 22:39:00 +0700

    chore: fix redirect

--- projects/space/src/app/app.routes.ts
+++ projects/space/src/app/app.routes.ts
@@ -4,7 +4,7 @@ export const routes: Routes = [
     {
         path: '',
         pathMatch: 'full',
-        redirectTo: 'experience',
+        redirectTo: 'space',
     },
     {
         path: 'space',
